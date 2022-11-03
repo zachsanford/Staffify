de210752361f727a9de54619e72dcad4e2b87f6a
Zach Sanford <sanfordzachary@gmail.com>
2022-11-03 14:26:09 -0700

    Build page. Update on the API works.

--- Staffify/Models/Data/Employee.cs
+++ Staffify/Models/Data/Employee.cs
@@ -1,10 +1,19 @@
-﻿namespace Staffify.Models.Data;
+﻿using System.ComponentModel.DataAnnotations;
+namespace Staffify.Models.Data;
 
 public class Employee
 {
     public int Id { get; set; }
     public int EmployeeId { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Name is too long.")]
     public string Name { get; set; }
+    [Required]
+    [Phone]
+    [Display(Name = "Phone Number")]
     public string PhoneNumber { get; set; }
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
     public string EmailAddress { get; set; }
 }
